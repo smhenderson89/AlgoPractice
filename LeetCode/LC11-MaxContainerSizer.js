449a514955d68c0335c98eba27042957a3deda46
Scott Henderson <scott.henderson@vertmob.com>
2022-07-04 20:21:50 -0700

    LC11-MaxContainer solved

--- LeetCode/LC11-MaxContainerSizer.js
+++ LeetCode/LC11-MaxContainerSizer.js
@@ -30,53 +30,38 @@ n == height.length
 2 <= n <= 10**5
 0 <= height[i] <= 10**4
 
+Test
+
+[1,8,6,2,5,4,8,3,7] -> max is 49
+[2,3,4,5,18,17,6] -> max is 17
+
 -------Code ------
 
 */ 
 
 var maxArea = function(height) {
 
-	len = height.length;
-	// Initialize maxArea
-	// Setup left and right pointers
-	i = 0
-	j = len - 1
-	width = j 
-	left = height[i]
-	right = height[j]
-	output = width * Math.min(height[i], height[j])
-    console.log('Inital Area: ', output)
-	// while width != 1
-	while (width > 1) {
-		// check height[i + 1] vs. height[j - 1]
-        // determine which new boundary will have a greater area. 
-        // If both are equal, than advance left pointer
-		
-		newLeft = height[i + 1]
-		newRight = height[j - 1]
-        newLeftArea = Math.min(newLeft, right) * (width - 1)
-        newRightArea = Math.min(newRight, left) * (width - 1)
+    output = 0
 
-		if (newLeftArea > newRightArea || newLeftArea == newRightArea) {
-			i += 1 // advance left pointer index
+    let i = 0
+    let j = height.length - 1
+
+    // while left index pointer is less than right index pointer
+    while (i < j) {
+        shortLine = Math.min(height[i], height[j])
+        // Update output for either local area or keep existing
+        output = Math.max(output, shortLine * (j - i))
+
+        // Look for next smallest width
+        if (height[i] < height[j]) {
+            i++
         } else {
-            j -= 1  // advance right pointer index
-        }
-
-        // determine new height
-        width = j - 1
-        localArea = Math.max(newLeftArea, newRightArea)
-        console.log('localArea', localArea, 'width', width)
-        // console.log('Area chunk:', area, '@ wdith:', width, 'height', minHeight)
-		// check if area > maxArea
-		if (localArea > output) {
-			output = localArea
-            console.log('New Area found', output, 'width', width)
+            j--
         }
     }
-    return output 
+    return output
 };
 
-console.log(maxArea([1,0,0,0,0,0,0,2,2]))
+console.log(maxArea([2,3,4,5,18,17,6]))
 
 
