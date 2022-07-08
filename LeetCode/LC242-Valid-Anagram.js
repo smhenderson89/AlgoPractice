8f1119b199b870fb82f6f0aca0aef8b06b010cbe
Scott Henderson <scott.henderson@vertmob.com>
2022-07-07 20:10:09 -0700

    LC242 Valid Anagram

---
+++ LeetCode/LC242-Valid-Anagram.js
@@ -0,0 +1,41 @@
+/* Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+link: https://leetcode.com/problems/valid-anagram/
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+Output: true
+Example 2:
+
+Input: s = "rat", t = "car"
+Output: false
+ 
+
+Constraints:
+
+1 <= s.length, t.length <= 5 * 10**4
+s and t consist of lowercase English letters. */
+
+var isAnagram = function(s, t) {
+
+    // sort s & t in alphabetical order
+    // if s & t are equal, then they are anagrams, return true
+    // else, return false
+
+    s = s.split("").sort().join("")
+    t = t.split("").sort().join("")
+
+    console.log(s)
+    console.log(t)
+
+    if (s === t) {
+        return true
+    } else {
+        return false
+    }    
+};
+
+console.log(isAnagram("!cat","cat!"))
--- LeetCode/test.js
+++ LeetCode/test.js
@@ -1,6 +1,12 @@
 // string replace example
 
-s = "A*b$C#"
-sLower = s.toLowerCase();
-clean = sLower.replace(/[^0-9a-z]/gi,'')
-console.log(clean);
+// s = "A*b$C#"
+// sLower = s.toLowerCase();
+// clean = sLower.replace(/[^0-9a-z]/gi,'')
+// console.log(clean);
+
+// sort example for strings
+
+s = "anagram"
+s = s.split("").sort().join("")
+console.log(s);
