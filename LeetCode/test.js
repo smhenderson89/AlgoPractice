f3971205c76e12dde7be53043db3bc1dd52cd973
Scott Henderson <scott.henderson@vertmob.com>
2022-07-07 19:46:16 -0700

    LC125-Valid-Palindrome

--- LeetCode/test.js
+++ LeetCode/test.js
@@ -1,15 +1,6 @@
-// Linked Lists example
-l1 = [1,2,4] 
-l2 = [1,3,4]
-l3 = [1,1,2,3,4,4]
+// string replace example
 
-function ListNode(val, next) {
-        this.val = (val===undefined ? 0 : val)
-         this.next = (next===undefined ? null : next)
-}
-list1 = {val: 1, next : { val: 2, next: { val: 4, next: null}}}
-list2 = {val: 1, next : { val: 3, next: { val: 4, next: null}}}
-
-console.log(list1.val)
-list1 = list1.next
-console.log('update list1', list1)
+s = "A*b$C#"
+sLower = s.toLowerCase();
+clean = sLower.replace(/[^0-9a-z]/gi,'')
+console.log(clean);
