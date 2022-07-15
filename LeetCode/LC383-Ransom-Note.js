597fe4e3f372ba5c0a37e63ca86f9c097b82caf7
Scott Henderson <scott.henderson@vertmob.com>
2022-07-14 21:37:02 -0700

    Ransom Note done with object

--- LeetCode/LC383-Ransom-Note.js
+++ LeetCode/LC383-Ransom-Note.js
@@ -22,4 +22,58 @@ Constraints:
 
 1 <= ransomNote.length, magazine.length <= 10**5
 ransomNote and magazine consist of lowercase English letters.
-*/ 
+*/
+
+// ----CODE ---
+
+// Edge Cases: 
+    
+
+
+// Iterate over ransomNote, 
+    // For each character, note[i], check if character exists in Map
+    // If so, subtract count by -1
+    // If character is required and count == 0, then return false
+    // Else return true
+
+var canConstruct = function(ransomNote, magazine) {
+    noteLen = ransomNote.length;
+    magLen = magazine.length;
+
+    // If magazine string length is 0 & ransomNote.length > 1, return false
+    if (magLen == 0 && noteLen > 1) {
+        return false 
+    // If ransomNote.length > magazine.length, return false
+    } else if (noteLen > magLen) {
+        return false
+    } else {
+        // Iterate over magainze string, create Map object and scan over magazine to create library
+        letterCounts = new Map();
+        for (let i =0; i < magLen; i++) {
+            // use letterCounts to keep track of leters in magazine
+            if (letterCounts.get(magazine[i])) { // if letter already has a count
+                count = letterCounts.get(magazine[i]) + 1 // increase count by 1
+                letterCounts.set(magazine[i], count) // update letterCounts
+            } else { // if letter doesn't exist in letterCounts
+                letterCounts.set(magazine[i], 1) // create intial count of 1
+            }
+        }
+
+        // console.log(letterCounts)
+
+        // Iterate over ransom note, check against letterCounts map
+        for (let j=0; j < noteLen; j++) {
+            letter = ransomNote[j]
+            if (letterCounts.get(ransomNote[j]) >= 1) {
+                count = letterCounts.get(ransomNote[j]) - 1
+                letterCounts.set(ransomNote[j], count)
+                // console.log(letterCounts)
+            } else {
+                return false
+            }
+        }
+        return true
+    }
+};
+
+console.log(canConstruct('givemethemoney','givemethemoneyg'))
--- LeetCode/test.js
+++ LeetCode/test.js
@@ -1,29 +1,17 @@
 // testing with hashtables
 
-hashMap = new Map();
+letterCounts = new Map();
 
-mag = 'aba';
+mag = 'abcdab';
 
-// hashMap.set(2, 7);
-// console.log(hashMap)
-// console.log(hashMap.get(2));
-// console.log(hashMap.has(2));
-
-for (let i=0; i < mag.length; i++) {
-    // If there is a collision
-    hash = mag.charCodeAt(i)
-
-    if (hashMap.get(hash)) { // if hash already exits
-        console.log('Collision with', mag[i])
-        // use hash as the basis for a linked list for all values 
-        // create an array table, using the hash as the key value
-        // the pair value(s) will be all elements that correspond to the same hash
-
-        // if linked list already exists, the add collision value to the existing linked list (list.push)
+for (let i =0; i < mag.length; i++) {
+    // use letterCounts to keep track of leters in mag
+    if (letterCounts.get(mag[i])) { // if letter already has a count
+        count = letterCounts.get(mag[i]) + 1 // increase count by 1
+        letterCounts.set(mag[i], count) // update letterCounts
+    } else { // if letter doesn't exist in letterCounts
+        letterCounts.set(mag[i], 1) // create intial count of 1
     }
-
-        
-    hashMap.set(hash, mag[i])
 }
 
-console.log(hashMap)
+console.log(letterCounts);
